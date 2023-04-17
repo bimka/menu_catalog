ab51c334715d11f0b63dfd9547f8d9351e0aabc8
Dmitriy Begishev <mintdragon@yandex.ru>
2023-04-17 20:59:28 +0500

    Change host name from 0.0.0.0 to db.

--- app/src/session.py
+++ app/src/session.py
@@ -1,17 +1,20 @@
 import os
+import socket
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
-
+# SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:postgres@172.21.0.2:5432/postgres"
+# addr = socket.gethostbyname('db')
+# SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://postgres:postgres@{addr}:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:postgres@db:5432/postgres"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
 
 
 def get_db():
