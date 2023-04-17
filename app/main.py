3aa63703dbd6ab2f4f3702ede6e896109be66169
Dmitriy Begishev <mintdragon@yandex.ru>
2023-04-17 20:59:53 +0500

    Added absolute importing.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
 
-from src.menus.router import menu_router
-from src.submenus.router import submenu_router
-from src.dishes.router import dish_router
+from .src.menus.router import menu_router
+from .src.submenus.router import submenu_router
+from .src.dishes.router import dish_router
 
 app = FastAPI()
 app.include_router(menu_router, prefix='/api/v1/menus')
@@ -12,5 +12,5 @@ app.include_router(dish_router, prefix='/api/v1/menus')
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1")
+    uvicorn.run(app, host="127.0.0.1:8000")
     
